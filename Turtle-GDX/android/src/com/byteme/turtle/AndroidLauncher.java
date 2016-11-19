518fb18fd2846f41280fe87df91f2b30c0ff115c
Kalih27 <zpqm1065@gmail.com>
2016-11-19 13:31:04 -0500

    Intital commit

---
+++ Turtle-GDX/android/src/com/byteme/turtle/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.byteme.turtle;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.byteme.turtle.Turle_Class;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Turle_Class(), config);
+	}
+}
