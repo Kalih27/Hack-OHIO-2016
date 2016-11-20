dbfe9c8b726b9f24fc85b54612a24fd27299256f
Kalih27 <zpqm1065@gmail.com>
2016-11-19 19:20:28 -0500

    Attempt to make turtle move

--- Turtle-GDX/core/src/com/byteme/turtle/Turle_Class.java
+++ Turtle-GDX/core/src/com/byteme/turtle/Turle_Class.java
@@ -4,30 +4,73 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
+
+
 
 public class Turle_Class extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	public class myActor extends Actor{
+		Texture img = new Texture("badlogic.png");
+
+		public boolean started = false;
+
+		public myActor(){
+			setBounds(getX(),getY(),img.getWidth(),img.getHeight());
+		}
+
+		@Override
+		public void draw(Batch batch, float alpha){
+			batch.draw(img,this.getX(),this.getY());
+		}
+	}
+	private Stage stage;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		myActor main = new myActor();
+
+		MoveToAction moveAction = new MoveToAction();
+		moveAction.setPosition(300f, 0f);
+		moveAction.setDuration(10f);
+		main.addAction(moveAction);
+
+		stage.addActor(main);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 960, 540);
-		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
 	}
 }
